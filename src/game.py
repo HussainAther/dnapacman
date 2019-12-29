f324fa214721f1e395d676d73205b6036a2fe04a
Syed Hussain Ather <shussainather@gmail.com>
2019-12-29 08:51:22 -0800

    refactor

--- src/game.py
+++ src/game.py
@@ -86,7 +86,10 @@ class Configuration:
         return (x + 13 * y) % sys.maxint
     
     def __str__(self):
-      return "(x,y)="+str(self.pos)+", "+str(self.direction)
+        """
+        Return the position and direction as a string.
+        """
+        return "(x,y)="+str(self.pos)+", "+str(self.direction)
     
     def generateSuccessor(self, vector):
         """
@@ -337,129 +340,138 @@ class Actions:
     getLegalNeighbors = staticmethod(getLegalNeighbors)
 
 class GameStateData:
-  """
-  Data for the game in its current state using these functions. 
-  """
-  def __init__( self, prevState = None ):
-    """ 
-    Generates a new data packet by copying information from its predecessor.
     """
-    if prevState != None:
-      self.food = prevState.food.shallowCopy()
-      self.capsules = prevState.capsules[:]
-      self.agentStates = self.copyAgentStates( prevState.agentStates )
-      self.layout = prevState.layout
-      self._eaten = prevState._eaten
-      self.score = prevState.score
-    self._foodEaten = None
-    self._capsuleEaten = None
-    self._agentMoved = None
-    self._lose = False
-    self._win = False
-    self.scoreChange = 0
-    
-  def deepCopy( self ):
-    state = GameStateData( self )
-    state.food = self.food.deepCopy()
-    state.layout = self.layout.deepCopy()
-    return state
-    
-  def copyAgentStates( self, agentStates ):
-    copiedStates = []
-    for agentState in agentStates:
-      copiedStates.append( agentState.copy() )
-    return copiedStates
-    
-  def __eq__( self, other ):
+    Data for the game in its current state using these functions. 
     """
-    Allows two states to be compared.
-    """
-    if other == None: return False
-    # TODO Check for type of other
-    if not self.agentStates == other.agentStates: return False
-    if not self.food == other.food: return False
-    if not self.capsules == other.capsules: return False
-    if not self.score == other.score: return False
-    return True
-                                                      
-  def __hash__( self ):
-    """
-    Allows states to be keys of dictionaries.
-    """
-    for i, state in enumerate( self.agentStates ):
-      try:
-        hash(state)
-      except TypeError as e:
-          print(e)
-          hash(state)
-    return hash(tuple(self.agentStates)) + 13*hash(self.food) + 113* hash(tuple(self.capsules)) + 7 * hash(self.score)
-
-  def __str__( self ): 
-    width, height = self.layout.width, self.layout.height
-    map = Grid(width, height)
-    for x in range(width):
-      for y in range(height):
-        food, walls = self.food, self.layout.walls
-        map[x][y] = self._foodWallStr(food[x][y], walls[x][y])
-    
-    for agentState in self.agentStates:
-      x,y = [int( i ) for i in nearestPoint( agentState.configuration.pos )]
-      agent_dir = agentState.configuration.direction
-      if agentState.isPacman:
-        map[x][y] = self._pacStr( agent_dir )
-      else:
-        map[x][y] = self._ghostStr( agent_dir )
-
-    for x, y in self.capsules:
-      map[x][y] = 'o'
-    
-    return str(map) + ("\nScore: %d\n" % self.score) 
+    def __init__( self, prevState = None ):
+        """ 
+        Generates a new data packet by copying information from its predecessor.
+        """
+        if prevState != None:
+            self.food = prevState.food.shallowCopy()
+            self.capsules = prevState.capsules[:]
+            self.agentStates = self.copyAgentStates( prevState.agentStates )
+            self.layout = prevState.layout
+            self._eaten = prevState._eaten
+            self.score = prevState.score
+        self._foodEaten = None
+        self._capsuleEaten = None
+        self._agentMoved = None
+        self._lose = False
+        self._win = False
+        self.scoreChange = 0
       
-  def _foodWallStr( self, hasFood, hasWall ):
-    if hasFood:
-      return '.'
-    elif hasWall:
-      return '%'
-    else:
-      return ' '
-    
-  def _pacStr( self, dir ):
-    if dir == Directions.NORTH:
-      return 'v'
-    if dir == Directions.SOUTH:
-      return '^'
-    if dir == Directions.WEST:
-      return '>'
-    return '<'
-    
-  def _ghostStr( self, dir ):
-    return 'G'
-    if dir == Directions.NORTH:
-      return 'M'
-    if dir == Directions.SOUTH:
-      return 'W'
-    if dir == Directions.WEST:
-      return '3'
-    return 'E'
-    
-  def initialize( self, layout, numGhostAgents ):
-    """
-    Creates an initial game state from a layout array (see layout.py).
-    """
-    self.food = layout.food.copy()
-    self.capsules = layout.capsules[:]
-    self.layout = layout
-    self.score = 0
-    self.scoreChange = 0
+    def deepCopy( self ):
+        state = GameStateData( self )
+        state.food = self.food.deepCopy()
+        state.layout = self.layout.deepCopy()
+        return state
+      
+    def copyAgentStates( self, agentStates ):
+        copiedStates = []
+        for agentState in agentStates:
+            copiedStates.append( agentState.copy() )
+        return copiedStates
+      
+    def __eq__( self, other ):
+        """
+        Allows two states to be compared.
+        """
+        if other == None: return False
+        # TODO Check for type of other
+        if not self.agentStates == other.agentStates: return False
+        if not self.food == other.food: return False
+        if not self.capsules == other.capsules: return False
+        if not self.score == other.score: return False
+        return True
+                                                        
+    def __hash__( self ):
+        """
+        Allows states to be keys of dictionaries.
+        """
+        for i, state in enumerate( self.agentStates ):
+            try:
+                hash(state)
+            except TypeError as e:
+                print(e)
+                hash(state)
+        return hash(tuple(self.agentStates)) + 13*hash(self.food) + 113* hash(tuple(self.capsules)) + 7 * hash(self.score)
+  
+      def __str__( self ): 
+          width, height = self.layout.width, self.layout.height
+          map = Grid(width, height)
+          for x in range(width):
+              for y in range(height):
+                  food, walls = self.food, self.layout.walls
+                  map[x][y] = self._foodWallStr(food[x][y], walls[x][y])
+      
+          for agentState in self.agentStates:
+              x,y = [int( i ) for i in nearestPoint( agentState.configuration.pos )]
+              agent_dir = agentState.configuration.direction
+              if agentState.isPacman:
+                  map[x][y] = self._pacStr( agent_dir )
+              else:
+                  map[x][y] = self._ghostStr( agent_dir )
+  
+          for x, y in self.capsules:
+              map[x][y] = 'o'
+      
+          return str(map) + ("\nScore: %d\n" % self.score) 
         
-    self.agentStates = []
-    numGhosts = 0
-    for isPacman, pos in layout.agentPositions:
-      if not isPacman: 
-        if numGhosts == numGhostAgents: continue # Max ghosts reached already
-        else: numGhosts += 1
-      self.agentStates.append( AgentState( Configuration( pos, Directions.STOP), isPacman) )
-    self._eaten = [False for a in self.agentStates]
+    def _foodWallStr( self, hasFood, hasWall ):
+        """
+        Check if the wall has food.
+        """
+        if hasFood:
+            return "."
+        elif hasWall:
+            return "%"
+        else:
+            return " "
+      
+    def _pacStr( self, dir ):
+        """
+        Return the direction in string form.
+        """
+        if dir == Directions.NORTH:
+            return "v"
+        if dir == Directions.SOUTH:
+            return "^"
+        if dir == Directions.WEST:
+            return ">"
+        return "<"
+      
+    def _ghostStr( self, dir ):
+        """
+        Return a string of the ghost.
+        """
+        return "G"
+        if dir == Directions.NORTH:
+            return "M"
+        if dir == Directions.SOUTH:
+            return "W"
+        if dir == Directions.WEST:
+            return "3"
+        return 'E'
+      
+    def initialize( self, layout, numGhostAgents ):
+      """
+      Creates an initial game state from a layout array (see layout.py).
+      """
+      self.food = layout.food.copy()
+      self.capsules = layout.capsules[:]
+      self.layout = layout
+      self.score = 0
+      self.scoreChange = 0
+          
+      self.agentStates = []
+      numGhosts = 0
+      for isPacman, pos in layout.agentPositions:
+        if not isPacman: 
+          if numGhosts == numGhostAgents: continue # Max ghosts reached already
+          else: numGhosts += 1
+        self.agentStates.append( AgentState( Configuration( pos, Directions.STOP), isPacman) )
+      self._eaten = [False for a in self.agentStates]
 
 class Game:
     """
