39e3004482ce04a27815926fec76ed458cb3365c
Syed Hussain Ather <shussainather@gmail.com>
2019-12-30 03:21:20 -0800

    cmments

--- src/ghostAgents.py
+++ src/ghostAgents.py
@@ -4,21 +4,42 @@ from game import Actions, Agent
 from util import chooseFromDistribution, manhattanDistance
 
 class RandomGhost( Agent ):
+    """
+    For the ghosts that move randomly, program how they
+    get an action from a set of legal actions.
+    """
     def __init__( self, index ):
+        """
+        Initialize the index of locations on the board.
+        """
         self.index = index
     
   def getAction( self, state ):
-    return random.choice( state.getLegalActions( self.index ) )
+      """
+      Get a random action from the possible (legal) actions.
+      """ 
+      return random.choice( state.getLegalActions( self.index ) )
   
   def getDistribution( self, state ):
-    actions = state.getLegalActions( self.index )
-    prob = 1.0 / len( actions )
-    return [( prob, action ) for action in actions]
+      """
+      Get the distribution of possible actions.
+      """
+      actions = state.getLegalActions( self.index )
+      prob = 1.0 / len( actions )
+      return [( prob, action ) for action in actions]
 
 class DirectionalGhost( Agent ):
-  def __init__( self, index, prob_attack=0.8, prob_scaredFlee=0.1 ):
-    self.index = index
-    self.prob_attack = prob_attack
+     """
+     For the ghosts that move in a specific direction relative
+     to the player, figure out how they will move.
+     """
+    def __init__( self, index, prob_attack=0.8, prob_scaredFlee=0.1 ):
+        """
+        Initialize the index (board location) and the probabilities the ghost
+        will attack or run away from Pac-Man.
+        """
+        self.index = index
+        self.prob_attack = prob_attack
     self.prob_scaredFlee = prob_scaredFlee
     
   def getAction( self, state ):
